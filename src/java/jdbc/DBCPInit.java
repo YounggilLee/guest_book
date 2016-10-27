0edc79ae5532c5f1e49e51575e900ea2b3494a3d
YounggilLee <clickbabo@gmail.com>
2016-10-27 12:10:27 -0400

    create database(name is guestbook), DBCPlint class to set connection pool (loading jdbc driver and initial connection pool), register DBCPInit class in web.xml file to excute the DBCInit class

---
+++ src/java/jdbc/DBCPInit.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jdbc;
+
+import java.sql.DriverManager;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import org.apache.tomcat.dbcp.dbcp2.ConnectionFactory;
+import org.apache.tomcat.dbcp.dbcp2.DriverManagerConnectionFactory;
+import org.apache.tomcat.dbcp.dbcp2.PoolableConnection;
+import org.apache.tomcat.dbcp.dbcp2.PoolableConnectionFactory;
+import org.apache.tomcat.dbcp.dbcp2.PoolingDriver;
+import org.apache.tomcat.dbcp.pool2.impl.GenericObjectPool;
+import org.apache.tomcat.dbcp.pool2.impl.GenericObjectPoolConfig;
+
+/**
+ *
+ * @author yglee
+ */
+public class DBCPInit extends HttpServlet {
+
+	@Override
+	public void init() throws ServletException {
+		loadJDBCDriver();
+		initConnectionPool();
+	}
+
+	private void loadJDBCDriver() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException ex) {
+			throw new RuntimeException("Fail to Load JDBC Driver", ex);
+		}
+	}
+
+	private void initConnectionPool() {
+		try {
+			String jdbcUrl = "jdbc:mysql://localhost:3306/guestbook?" + 
+                                          "useUnicode=true&characterEncoding=utf8";
+			String username = "root";
+			String pw = "1234";
+
+			ConnectionFactory connFactory = 
+					new DriverManagerConnectionFactory(jdbcUrl, username, pw);
+
+			PoolableConnectionFactory poolableConnFactory = 
+					new PoolableConnectionFactory(connFactory, null);
+			poolableConnFactory.setValidationQuery("select 1");
+
+			GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+			poolConfig.setTimeBetweenEvictionRunsMillis(1000L * 60L * 5L);
+			poolConfig.setTestWhileIdle(true);
+			poolConfig.setMinIdle(4);
+			poolConfig.setMaxTotal(50);
+
+			GenericObjectPool<PoolableConnection> connectionPool = 
+					new GenericObjectPool<>(poolableConnFactory, poolConfig);
+			poolableConnFactory.setPool(connectionPool);
+			
+			Class.forName("org.apache.commons.dbcp2.PoolingDriver");
+			PoolingDriver driver = 
+					(PoolingDriver) DriverManager.getDriver("jdbc:apache:commons:dbcp:");
+			driver.registerPool("guestbook", connectionPool);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
+
